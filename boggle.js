4b235ce718ace8e78b6b99238039b73c2b7f5f03
Satchel Moore <samoore@davidson.edu>
2020-02-03 16:16:50 -0800

    edited

--- boggle.js
+++ boggle.js
@@ -7,7 +7,7 @@ function newBoggleBoard() {
   ];
 }
 
-/**
+/**ls
  * Prints out a boggle board.
  */
 function printBoggleBoard(boggleBoard) {
@@ -16,13 +16,82 @@ function printBoggleBoard(boggleBoard) {
   }
 }
 
-/**
- * Shake a boggle board and fill it with letters.
- */
-function shake(boggleBoard) {
-  // This is your job. :)
-  return boggleBoard;
+
+// function shake(boggleBoard) {
+// let abc = 'abcdefghijklmnopqrstuvwxyz';
+//   for(let row of boggleBoard){
+//     for(let i = 0; i < boggleBoard.length; i++){
+//       row[i] = abc[Math.floor(Math.random()*26)];
+//     }
+
+//   }
+//   return boggleBoard;
+
+// }
+function shake(boggleBoard){
+  let dice =  [
+    {
+      combo: 'AAEEGN',
+    },
+    {
+      combo: 'ELRTTY',
+    },
+    {
+      combo: 'AOOTTW',
+    },
+    {
+      combo: 'ABBJOO',
+    },
+    {
+      combo: 'EHRTVW',
+    },
+    {
+      combo: 'CIMOTU',
+    },
+    {
+      combo: 'DISTTY',
+    },
+    {
+      combo: 'EIOSST',
+    },
+    {
+      combo: 'DELRVY',
+    },
+    {
+      combo: 'ACHOPS',
+    },
+    {
+      combo: 'HIMNQU',
+    },
+    {
+      combo: 'EEINSU',
+    },
+    {
+      combo: 'EEGHNW',
+    },
+    {
+      combo: 'AFFKPS',
+    },
+    {
+      combo: 'HLNNRZ',
+    },
+    {
+      combo: 'DEILRX',
+    },
+
+
+  ]
+  for(let row of boggleBoard){
+  for(let i = 0; i<boggleBoard.length;i++){
+  let randomDice = Math.floor(Math.random() * 15);
+  let randomLetter = Math.floor(Math.random() * 5);
+  row[i]=dice[randomDice].combo[randomLetter];
+
+  }
 }
+return boggleBoard;
+}
+
 
 let board = newBoggleBoard();
 
